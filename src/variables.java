b141c9d65296323e7373b7eece18e8138a551375
jennl97 <jennhott97@gmail.com>
2017-09-06 21:01:53 -0500

    Initial commit

---
+++ src/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args){
+        byte jennsByte = 3;
+        short jennsShort = 1;
+        int jennsInt = 42;
+        long jennsLong = 13699785L;
+        float jennsFloat = 3.14f;
+        double jennsDouble = 3.14159;
+        boolean jennsBoolean = true;
+        char jennsChar = 'c';
+
+        System.out.println("Byte:" + jennsByte);
+        System.out.println("Short:" + jennsShort);
+        System.out.println("Int:" + jennsInt);
+        System.out.println("Long:" + jennsLong);
+        System.out.println("Float:" + jennsFloat);
+        System.out.println("Double:" + jennsDouble);
+        System.out.println("Boolean:" + jennsBoolean);
+        System.out.println("Char:" + jennsChar);
+    }
+}
